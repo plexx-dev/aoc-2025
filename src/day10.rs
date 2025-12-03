14746615629f7157b17c6a7620d7b933c7426631
plexx-dev <plexx@plexx.dev>
2025-12-03 23:05:38 +0100

    added cli argument to select day to solve

---
+++ src/day10.rs
@@ -0,0 +1,38 @@
+use std::{fs::File, io::Read, path::Path};
+
+pub fn solve() -> (i32, i32) {
+    (part1(), part2())
+}
+
+fn get_puzzle_input() -> String {
+    let path = Path::new("input/day1.txt");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("konnte {} nicht öffnen: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+
+    s
+}
+
+fn part1() -> i32 {
+    let mut solution = 0;
+    let s = get_puzzle_input();
+
+    for line in s.lines() {}
+
+    solution
+}
+
+fn part2() -> i32 {
+    let mut solution = 0;
+    let s = get_puzzle_input();
+
+    for line in s.lines() {}
+
+    solution
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
 mod day1;
 mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
+mod day7;
+mod day8;
+mod day9;
+mod day10;
+mod day11;
+mod day12;
 
 fn main() {
-    println!("Day 1: {:?}", day1::solve());
-    println!("Day 2: {:?}", day2::solve());
+    let day = get_day_from_args();
+
+    let solvers: [fn() -> Box<dyn std::fmt::Debug>; 12] = [
+        || Box::new(day1::solve()),
+        || Box::new(day2::solve()),
+        || Box::new(day3::solve()),
+        || Box::new(day4::solve()),
+        || Box::new(day5::solve()),
+        || Box::new(day6::solve()),
+        || Box::new(day7::solve()),
+        || Box::new(day8::solve()),
+        || Box::new(day9::solve()),
+        || Box::new(day10::solve()),
+        || Box::new(day11::solve()),
+        || Box::new(day12::solve()),
+    ];
+
+    println!("Day {}: {:?}", day, solvers[day - 1]());
+}
+
+fn get_day_from_args() -> usize {
+    std::env::args()
+        .nth(1)
+        .expect("Missing day argument")
+        .parse::<usize>()
+        .expect("Not a number")
+        .clamp(1, 12)
 }
