f853c883282828bf85d553d62c11ea8ae7b6c2c5
plexx-dev <plexx@plexx.dev>
2025-12-04 22:14:58 +0100

    day 4

--- src/day4.rs
+++ src/day4.rs
@@ -5,7 +5,7 @@ pub fn solve() -> (i32, i32) {
 }
 
 fn get_puzzle_input() -> String {
-    let path = Path::new("input/day1.txt");
+    let path = Path::new("input/day4.txt");
     let display = path.display();
 
     let mut file = match File::open(&path) {
@@ -19,11 +19,46 @@ fn get_puzzle_input() -> String {
     s
 }
 
+fn check_space_around(grid: &Vec<Vec<bool>>, x: usize, y: usize) -> bool {
+    let mut amount = 0;
+    for i in -1isize..=1 {
+        if y == 0 && i == -1 || y == grid.len() - 1 && i == 1 {
+            continue;
+        }
+        if x > 0 && grid[(y as isize + i) as usize][x - 1] {
+            amount += 1;
+        }
+        if x < grid[y].len() - 1 && grid[(y as isize + i) as usize][x + 1] {
+            amount += 1;
+        }
+        if i != 0 && grid[(y as isize + i) as usize][x] {
+            amount += 1;
+        }
+    }
+
+    if amount < 4 { true } else { false }
+}
+
 fn part1() -> i32 {
     let mut solution = 0;
     let s = get_puzzle_input();
 
-    for line in s.lines() {}
+    let grid: Vec<Vec<bool>> = s
+        .lines()
+        .map(|x| {
+            x.chars()
+                .map(|char| if char == '.' { false } else { true })
+                .collect()
+        })
+        .collect();
+
+    for (y, row) in grid.iter().enumerate() {
+        for (x, field) in row.iter().enumerate() {
+            if *field && check_space_around(&grid, x, y) {
+                solution += 1;
+            }
+        }
+    }
 
     solution
 }
@@ -32,7 +67,29 @@ fn part2() -> i32 {
     let mut solution = 0;
     let s = get_puzzle_input();
 
-    for line in s.lines() {}
+    let mut grid: Vec<Vec<bool>> = s
+        .lines()
+        .map(|x| {
+            x.chars()
+                .map(|char| if char == '.' { false } else { true })
+                .collect()
+        })
+        .collect();
+
+    let mut last_solution = -1;
+    while last_solution != solution {
+        last_solution = solution;
+        let last_grid = grid.clone();
+
+        for y in 0..grid.len() {
+            for x in 0..grid[y].len() {
+                if last_grid[y][x] && check_space_around(&last_grid, x, y) {
+                    grid[y][x] = false;
+                    solution += 1;
+                }
+            }
+        }
+    }
 
     solution
 }
